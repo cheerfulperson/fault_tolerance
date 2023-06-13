f7428f0065de46436960296b2df65d6766e648d5
yar0m4 <belboroda3@mail.ru>
2023-06-14 00:41:32 +0300

    Update Route

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,3 +4,4 @@ const String firstAppRoute = '/firstAppRoute';
 const String firstAppDataRoute = '/firstAppDataRoute';
 
 const String secondAppRoute = '/secondAppRoute';
+const String secondAppRouteData = '/secondAppRouteData';
